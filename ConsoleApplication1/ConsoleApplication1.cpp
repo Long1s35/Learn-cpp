164de02b72dca585c812ea9da74d7e403425f7a5
MatveyBelaev2115 <matvejbelaev50@gmail.com>
2024-10-08 19:17:58 +0300

    Added new project.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class CLift {
+	int mFf, mLf;
+public:
+	void FirstF(int ff) {
+		mFf = ff;
+	}
+	void LastF(int lf) {
+		mLf = lf;
+	}
+	int GetFf() {
+		return mFf;
+	}
+	int GetLf() {
+		return mLf;
+	}
+	string GetString() {
+		return "Лифт ездит с " + to_string(mFf) + " по " + to_string(mLf) + " этаж.";
+	}
+	void Print() {
+		cout << GetString();
+	}
+	CLift(int ff, int lf) {
+		mFf = ff;
+		mLf = lf;
+	}
+	~CLift() {};
+};
+int main() {
+	setlocale(LC_ALL, "");
+	CLift lift = CLift(1, 9);
+	lift.Print();
+}
