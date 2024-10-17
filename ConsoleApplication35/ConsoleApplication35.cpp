50485f83e3ed12ff53d4bec9f990111e2b1e174e
MatveyBelaev2115 <golubevdanil2009@gmail.com>
2024-10-17 18:25:11 +0300

    Create ConsoleApplication35.cpp
    Добавил Класс Number, внес переменную, сеттер и геттер

---
+++ ConsoleApplication35/ConsoleApplication35.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Number
+{
+	int mNumber;
+public:
+	void SetNumb(int number) { mNumber = number; }
+	int GetNumb() { return mNumber; }
+
+private:
+
+};
+
+
+int main()
+{
+
+}
